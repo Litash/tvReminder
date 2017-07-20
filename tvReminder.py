c8d12f1cf92f68ce79ac9c5e9f6747fb557dfcab
Yichen Lu <litash529@gmail.com>
2017-07-20 22:08:03 +0100

    errors caused by website changes are fixed

--- tvReminder.py
+++ tvReminder.py
@@ -1,12 +1,24 @@
 # -*- coding: UTF-8 -*-
-import gspread, requests, schedule, time, datetime
+'''
+tested on python 2.7
+Author: Yichen Lu
+Email: me@yichen.Lu
+www.yichen.lu
+'''
+import gspread
+import requests
+import schedule
+import time
+import datetime
 from oauth2client.service_account import ServiceAccountCredentials
 from bs4 import BeautifulSoup
+
 
 def job():
     scope = ['https://spreadsheets.google.com/feeds']
 
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('tvReminder-9c2fc76b87b6.json', scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        'tvReminder-9c2fc76b87b6.json', scope)
 
     gc = gspread.authorize(credentials)
 
@@ -22,8 +34,8 @@ def job():
     for x in xrange(len(dlurl_cells)):
         dlurl = dlurl_cells[x].value
         if dlurl is not '':
-            resurl = dlurl.replace('/list','')
-            # print resurl
+            resurl = dlurl.replace('/resource/list/', '/gresource/')
+            print(resurl)
 
             try:
                 # visit zimuzu.com
@@ -34,8 +46,9 @@ def job():
                 note = soup.select(".resource-tit > p")
                 notestr = note[0].string
                 # print notestr
-                replaceRawStr = unicode('說明：','utf-8')
-                newNoteStr = notestr.replace(replaceRawStr,'')
+                replaceRawStr = unicode('說明：', 'utf-8')
+                newNoteStr = notestr.replace(replaceRawStr, '')
+                # print(newNoteStr)
 
                 if not note_cells[x].value == newNoteStr:
                     isUpdate = 1
@@ -44,16 +57,21 @@ def job():
                     update_cells[x].value = now.strftime("%Y-%m-%d")
 
             except Exception as e:
-                print(e)
+                if str(e) == "list index out of range":
+                    print(e)
+                    isUpdate = 1
+                    note_cells[x].value = "Download link may be broken"
+                    update_cells[x].value = "Error"
                 continue
 
     # Update in batch
-    if isUpdate==1 :
+    if isUpdate == 1:
         worksheet.update_cells(note_cells)
         worksheet.update_cells(update_cells)
 
 if __name__ == '__main__':
     job()
+    # schedule job
     schedule.every(4).hours.do(job)
     while True:
         schedule.run_pending()
